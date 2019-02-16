df6519e92aea09b297fffeb0e37ae51ba6410b4d
Romane PICOUT <romanepicout@gmail.com>
2019-02-16 16:34:01 +0100

    Modification de l'ihm'

--- src/main/resources/static/listeJS.js
+++ src/main/resources/static/listeJS.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$('#tableauEtablissement').DataTable();
-} );
+});
+
+collapseRow = function(){
+	var row = document.getElementById("collapseRow");
+	row.hidden(false);
+}
