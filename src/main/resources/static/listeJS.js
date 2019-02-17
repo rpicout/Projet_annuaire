cb52ba44f4edbe7c55a4171dd5ff88a8d34dc789
Romane PICOUT <romanepicout@gmail.com>
2019-02-17 01:08:40 +0100

    Séparation de la liste des établissements et de la liste des établissements par code postal

--- src/main/resources/static/listeJS.js
+++ src/main/resources/static/listeJS.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
 	$('#tableauEtablissement').DataTable();
 });
-
-collapseRow = function(){
-	var row = document.getElementById("collapseRow");
-	row.hidden(false);
-});
-
-$(document).ready(function() {
-	$('#tableauCodePostal').DataTable();
-} );
