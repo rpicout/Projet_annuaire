a5d0ea2e9470a1a6af20c9d3aa22aa2cc3d2785c
Romane PICOUT <romanepicout@gmail.com>
2019-02-19 22:50:23 +0100

    Mise à jour de l'url de la page en fonction du code postal qu'on entre

--- src/main/resources/static/listeJS.js
+++ src/main/resources/static/listeJS.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	$('#tableauEtablissement').DataTable();
+	$('#tableauEtablissement').DataTable( {
+        responsive: true,
+        "pageLength": 10
+    } );
 });
+
+$(document).ready(function() {
+	$('#tableauCodePostal').DataTable( {
+        responsive: true,
+        "pageLength": 10
+    } );
+});
+
+var reloadUrl = function() {
+	var cp = document.getElementById("codePostal").value; 
+	console.log(cp);
+	window.location.href = "http://localhost:8080/rechercheCodePostal"+cp;
+};
